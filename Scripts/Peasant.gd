586b51dca2fa5dcff35864fb4fcf19c01be2ba85
Vincent Sietses <vincent.sietses@aerospacelab.be>
2021-08-21 16:37:14 +0200

    Three paths and collision

--- Scripts/Peasant.gd
+++ Scripts/Peasant.gd
@@ -4,8 +4,9 @@ extends KinematicBody2D
 onready var path_follow = get_parent()
 
 var move_direction = 0
-var speed = 30
+var speed = 50
 var time_since_last_anim : float = 0
+var time_between_animations : float = rand_range(0.15, 0.4)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,8 +14,8 @@ func _ready():
 
 func _physics_process(delta):
 	time_since_last_anim += delta
-	if time_since_last_anim > 0.7 :
-		time_since_last_anim = fmod(time_since_last_anim, 0.7)
+	if time_since_last_anim > time_between_animations :
+		time_since_last_anim = fmod(time_since_last_anim, time_between_animations)
 		var rot = 18*randf()
 		if rotation_degrees < 0 : 
 			rotation_degrees = rot
@@ -27,4 +28,7 @@ func MovementLoop(delta : float) :
 	path_follow.set_offset(path_follow.get_offset() + speed * delta)
 	var pos = path_follow.get_global_position()	
 	move_direction = (pos.angle_to_point(prepos) / PI)*180
-	
+	if move_direction <= 90 and move_direction >= - 90 : 
+		scale.x = -1
+	else:
+		scale.x = 1
--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,10 +1,6 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var wave_number : int = 0 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
